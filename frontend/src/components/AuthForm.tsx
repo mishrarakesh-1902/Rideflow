02e821797d18ef841f3853f3b291b39ffcdc0aa1
mishrarakesh-1902 <mishrarakeshkumar766@gmail.com>
2025-11-02 12:25:03 +0530

    updated

--- frontend/src/components/AuthForm.tsx
+++ frontend/src/components/AuthForm.tsx
@@ -52,7 +52,8 @@ const AuthForm: React.FC = () => {
   // ---------------- Login ----------------
   const handleLogin = async () => {
     try {
-      const response = await axios.post("http://localhost:5000/api/auth/login", {
+      // const response = await axios.post("http://localhost:5000/api/auth/login", {
+      const response = await axios.post("https://rideflow1.onrender.com/api/auth/login", {
         email: formData.email,
         password: formData.password
       });
@@ -90,7 +91,7 @@ const AuthForm: React.FC = () => {
         return;
       }
 
-      const response = await axios.post("http://localhost:5000/api/auth/signup", {
+      const response = await axios.post("https://rideflow1.onrender.com/api/auth/signup", {
         name: formData.name,
         email: formData.email,
         phone: formData.phone,
